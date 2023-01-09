7095381323e2170eaf26d0e6fa41258ff37a3679
Salih <salihensari91@gmail.com>
2023-01-08 23:24:31 -0500

    Controller added.

---
+++ LeaveManagement.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurations;
+
+public class MapperConfig : Profile
+{
+    public MapperConfig()
+    {
+
+        CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+    }
+
+}
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        public string Name { get; set; }
+
+
+
+        [Display(Name = " Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
